89ca7a89f317419ac69e7f19c9b41751d22cdb85
Gregory Singleton <yadig69design@gmail.com>
2020-07-01 23:35:11 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,46 @@
+// let gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGift(gift) {
+//   console.log(`wrapped ${gift} and added a bow!`);
+// }
+
+// wrapGift(gifts[0]);
+// wrapGift(gifts[1]);
+// wrapGift(gifts[2]);
+
+// for (let num = 1; num < 6; num += 1) {
+//   console.log(num)
+// }
+
+// for (let i = 1; i < 100; i++) {
+//   console.log("hello world the " + i + "time");
+// }
+
+// let gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGift(gifts) {
+// for (let i = 0; i < gifts.length; i++) {
+//   console.log(`wrapped ${gifts[i]} and added a bow!`);
+//   }
+// }
+
+// wrapGift(gifts)
+
+function forLoop(array){
+for (i = 0; i < 25; i++) {
+if(i === 1) {
+array.push('I am 1 strange loop.')
+} else {
+array.push(`I am ${i} strange loops.`)
+}
+}
+return array;
+}
+
+function whileLoop(n) {
+  while (n = 100) {
+    console.log(-n)
+    n = 0
+  }
+  return"done";
+}
